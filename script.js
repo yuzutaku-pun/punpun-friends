51050987455c71d9dd6b80270b53119502486fc3
yuzutaku-pun <bv6p5t8zh8@privaterelay.appleid.com>
2026-06-21 05:50:29 +0900

    Update alerts and initialize Supabase client

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-alert("1");
+alert("A");
 
-alert(typeof window.supabase);
+const supabase = window.supabase.createClient(
+    "https://snwzkkruiopjahswrwos.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3pra3J1aW9wamFoc3dyd29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NzUzMDIsImV4cCI6MjA5NzU1MTMwMn0.Db7MKZBE5m9IAIi2C3uCYY6KgMcWnltuR2hl4mSRV6Q"
+);
 
-alert("2");
+alert("B");
+
+const addBtn = document.getElementById("addBtn");
+
+addBtn.onclick = async () => {
+
+    alert("C");
+
+    const { data, error } =
+        await supabase
+            .from("users")
+            .select("*");
+
+    alert("D");
+
+    if(error){
+        alert(error.message);
+        return;
+    }
+
+    alert(JSON.stringify(data));
+
+};
