b14aabd508390b0e1fe24a449e3dfc6cf7763146
yuzutaku-pun <bv6p5t8zh8@privaterelay.appleid.com>
2026-06-21 05:30:34 +0900

    Simplify button click event assignment
    Removed conditional check for addBtn and directly assigned the onclick event.

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ alert("読み込み成功");
 
 const addBtn = document.getElementById("addBtn");
 
-if (addBtn) {
-    addBtn.onclick = () => {
-        alert("ボタン成功");
-    };
-} else {
-    alert("addBtnが見つかりません");
-}
+addBtn.onclick = () => {
+    alert("ボタン成功");
+};
