b91497244cdd10c880ae6b96a2b31539f7f8a3ba
yuzutaku-pun <bv6p5t8zh8@privaterelay.appleid.com>
2026-06-21 06:53:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,69 +3,28 @@ const supabase = window.supabase.createClient(
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNud3pra3J1aW9wamFoc3dyd29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NzUzMDIsImV4cCI6MjA5NzU1MTMwMn0.Db7MKZBE5m9IAIi2C3uCYY6KgMcWnltuR2hl4mSRV6Q"
 );
 
+const addBtn = document.getElementById("addBtn");
 const uidInput = document.getElementById("uid");
-const joinBtn = document.getElementById("joinBtn");
 
-joinBtn.onclick = async () => {
+addBtn.onclick = async () => {
 
-  const uid = uidInput.value.trim();
+  const user = uidInput.value.trim();
 
-  if (!uid) return;
+  if (!user) {
+    alert("UIDを入力");
+    return;
+  }
 
-  localStorage.setItem("uid", uid);
+  const { error } = await supabase
+    .from("users")
+    .insert([
+      { user: user }
+    ]);
 
-  await updateOnline(uid);
+  if (error) {
+    alert("保存失敗: " + error.message);
+    return;
+  }
 
-  setInterval(() => {
-    updateOnline(uid);
-  }, 30000);
-
+  alert("保存成功");
 };
-
-async function updateOnline(uid){
-
-  await supabase
-    .from("users")
-    .upsert({
-      uid: uid,
-      last_seen: new Date().toISOString()
-    });
-
-    async function loadUsers(){
-
-  const { data } = await supabase
-    .from("users")
-    .select("*");
-
-  const list =
-    document.getElementById("onlineList");
-
-  list.innerHTML = "";
-
-  data.forEach(user => {
-
-    const diff =
-      Date.now() -
-      new Date(user.last_seen).getTime();
-
-    const online =
-      diff < 60000;
-
-    const div =
-      document.createElement("div");
-
-    div.textContent =
-      (online ? "🟢 " : "🔴 ")
-      + user.uid;
-
-    list.appendChild(div);
-
-  });
-
-}
-
-setInterval(loadUsers,5000);
-loadUsers();
-    
-
-}
